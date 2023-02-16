a4a57d2d7a41565c93e7ba1349c4a10704fe4852
samuelkd1 <samuel.ofosuaah@outlook.com>
2023-02-16 17:44:55 +0000

    made a new tk label

--- hello.py
+++ hello.py
@@ -11,10 +11,13 @@ from tkinter import *
 root = Tk()
 # youve made a label
 myLabel = Label(root, text = "Hello World!")
+# this is a 2nd label to show to git
+myLabel2 = Label(root, text = "Hello Git!!!")
 
 # now to put it on the screen by packing
 
 myLabel.pack()
+myLabel2.pack()
 
 # now to create a event loop
 
